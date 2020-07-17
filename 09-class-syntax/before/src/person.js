de30a6f05e0c61aeb7bf6f5e1b3af4df96288c06
avrahamm <papaabramg@gmail.com>
2020-07-17 15:22:25 +0300

    react - lesson 9

--- 09-class-syntax/before/src/person.js
+++ 09-class-syntax/before/src/person.js
@@ -1,30 +1,37 @@
 import React from 'react';
-import { useState } from 'react';
-
 import _ from 'lodash';
+
+export default class Person extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = { mood: getRandomMood()}
+    this.changeMood = this.changeMood.bind(this)
+  }
+
+  changeMood() {
+    this.setState({mood: getRandomMood()});
+  }
+
+  render()
+  {
+    const {name} = this.props;
+    const {mood} = this.state;
+    const style = {color: moods[mood]}
+
+    return (
+        <p style={style}>
+          Hello. My name is {name} and I'm {mood}
+          <button onClick={this.changeMood} >Change Mood</button>
+        </p>
+  );
+}
+}
 
 Person.defaultProps = {
   name: 'Visitor',
 }
-
-export default function Person(props) {
-  const { name } = props;
-  const [mood, setMood] = useState(getRandomMood());
-
-  function changeMood() {
-    setMood(getRandomMood());
-  }
-
-  const style = { color: moods[mood] };
-  
-  return (
-    <p style={style}>
-      Hello. My name is {name} and I'm {mood}
-      <button onClick={changeMood}>Change Mood</button>
-    </p>
-  );
-}
-
 
 const moods = {
   happy: 'pink',
