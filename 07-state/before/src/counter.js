44e3c86af4c0423271233c57ccd8076b797ecb9a
avrahamm <papaabramg@gmail.com>
2020-07-17 14:24:46 +0300

    react - lesson 7

--- 07-state/before/src/counter.js
+++ 07-state/before/src/counter.js
@@ -2,17 +2,32 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function Counter(props) {
-  const count = 0;
-  
+  // const count = 0;
+  const [count, setCount] = useState(0)
+  const [delta, setDelta] = useState(1)
+
+    function inc() {
+      setCount( oldValue => oldValue + delta )
+    }
+
+    function reset() {
+      setCount(0)
+    }
+
+    function handleChangeDelta(e) {
+      setDelta(Number(e.target.value))
+    }
+
   return (
     <div>
       <label>
         Increase By:
-        <input type="number" />
+        <input type="number" value={delta} onChange={handleChangeDelta}/>
       </label>
       <p>
         I was clicked {count} times        
-        <button>Click Me</button>
+        <button onClick={inc}>Click Me</button>
+        <button onClick={reset}>Reset</button>
       </p>
     </div>
   );
