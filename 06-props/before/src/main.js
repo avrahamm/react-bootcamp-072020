d4ed46677905e46dafb0039cc50985376296416c
avrahamm <papaabramg@gmail.com>
2020-07-17 11:51:50 +0300

    react - lesson 6

--- 06-props/before/src/main.js
+++ 06-props/before/src/main.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import Person from './person';
 
 const App = () => {
-
-  return (
-    <div>
-      <Person name="bob" />
-    </div>
-  )
+    const colors = ['red', 'blue']
+    return (
+        <div>
+            <Person
+                // name="bob" age={20}
+                colors={colors}/>
+        </div>
+    )
 };
 
 
 // main.js
 const root = document.querySelector('main');
-ReactDOM.render(<App />, root);
+ReactDOM.render(<App/>, root);
--- 06-props/before/src/person.js
+++ 06-props/before/src/person.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default function Person(props) {
-  const { name } = props;
+    const {name} = props;
+    const nextYear = props.age + 1
+    return (
+        <>
+            <pre>{JSON.stringify(props)}</pre>
+            <h2>Hello. My name is {name}</h2>
+            <p>Next year {nextYear}</p>
+        </>
+    );
+}
 
-  return (
-    <h2>Hello. My name is {name}</h2>
-  );
+Person.defaultProps = {
+    name: 'guest',
+    age: 20
 }
