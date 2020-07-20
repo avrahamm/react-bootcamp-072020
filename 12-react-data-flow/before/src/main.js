5bfa5ac4496837b1ccd8b8129387e76db7e4e393
avrahamm <papaabramg@gmail.com>
2020-07-20 21:11:21 +0300

    react - lesson 12, 13

--- 12-react-data-flow/before/src/main.js
+++ 12-react-data-flow/before/src/main.js
@@ -1,64 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useState } from 'react';
+import {useState} from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
 
 function Display(props) {
-  const score = 2;
-
-  return (
-    <div style={{
-      background: '#d2d2d2',
-      padding: '10px 2px',
-      boxShadow: '0 0 1px 1px rgba(0,0,0,0.6)',
-    }}>
-      You scored:
-      <div style={{
-        display: 'inline-block',
-        background: 'black',
-        color: 'white',
-        padding: '10px',
-        fontFamily: 'Orbitron, sans-serif',
-      }}>{score}</div>
-      <button>Reset</button>
-    </div>
-  );
+    const {count, resetCounter, isMax} = props;
+    const bgColor = isMax ? "orange" : '#d2d2d2'
+    return (
+        <div className={'row'} style={{
+            background: bgColor,
+            padding: '10px 2px',
+            boxShadow: '0 0 1px 1px rgba(0,0,0,0.6)',
+        }}>
+            <div className={'col-1'}>You scored:</div>
+            <div className={'col-1'} style={{
+                display: 'inline-block',
+                background: 'black',
+                color: 'white',
+                padding: '10px',
+                fontFamily: 'Orbitron, sans-serif',
+            }}>{count}</div>
+            <button onClick={resetCounter}>Reset</button>
+        </div>
+    );
 }
 
 function Counter(props) {
-  const [ count, setCount ] = useState(0);
+    const {updateCounter, resetCounter, count, isMax} = props;
 
-  function inc() {
-    setCount(x => x + 1);
-  }
-
-  function reset() {
-    setCount(0);
-  }
-
-  return (
-    <div>
-      <pre>Debug: count = {count}</pre>
-
-      <Display />
-      <button onClick={inc}>Click Me</button>
-    </div>
-  );
+    return (
+        <div>
+            <Display count={count} resetCounter={resetCounter} isMax={isMax}/>
+            <button onClick={updateCounter}>Click Me</button>
+        </div>
+    );
 }
 
 const App = () => {
-  const [maxValue, setMaxValue] = useState(0);
+    const [counters, setCounters] = useState([0, 0, 0, 0]);
 
-  return (
-    <div>
-      <Counter  />
-      <Counter  />
-      <Counter  />
-      <Counter  />
-    </div>
-  )
+    function updateCounter(idx, func) {
+        const updatedCounters = [...counters]
+        updatedCounters[idx] = func(updatedCounters[idx]);
+        setCounters(updatedCounters)
+    }
+
+    function inc(val) {
+        return val + 1
+    }
+
+    function reset() {
+        return 0
+    }
+
+    const max = counters.reduce((acc, val) => val > acc ? val : acc)
+
+    return (
+        <div className={'container'}>
+            <Counter
+                updateCounter={() => updateCounter(0, inc)}
+                resetCounter={() => updateCounter(0, reset)}
+                count={counters[0]}
+                isMax={counters[0] === max}
+            />
+            <Counter
+                updateCounter={() => updateCounter(1, inc)}
+                resetCounter={() => updateCounter(1, reset)}
+                count={counters[1]} isMax={counters[1] === max}
+            />
+        </div>
+    )
 };
 
 
 // main.js
 const root = document.querySelector('main');
-ReactDOM.render(<App />, root);
+ReactDOM.render(<App/>, root);
--- 13-saving-array-or-object-in-state/before/src/main.js
+++ 13-saving-array-or-object-in-state/before/src/main.js
@@ -6,6 +6,25 @@ function SelectableList(props) {
   const { items } = props;
   const [selectedItems, setSelectedItems] = useState(new Set());
 
+  function toggleItem(add, item)
+  {
+      console.log(add, item)
+      console.log(`toggleItem: 1) selectedItems`)
+      console.log(selectedItems)
+
+      if( add) {
+          selectedItems.add(item)
+      } else {
+          selectedItems.delete(item)
+      }
+      console.log(`toggleItem: 2) selectedItems`)
+      console.log(selectedItems)
+      // let updatedArr = [ ...selectedItems ];
+      // let updatedArr = Array.from(selectedItems);
+      // let updatedSet = new Set(updatedArr)
+      setSelectedItems(new Set(selectedItems))
+  }
+
   return (
     <>
       <p>Selected Items: {Array.from(selectedItems).join(', ')}</p>
@@ -14,7 +33,8 @@ function SelectableList(props) {
           <li key={item} style={{ direction: "rtl" }}>
             <label>
               {item}
-              <input type="checkbox" />
+              <input type="checkbox" checked={selectedItems.has(item)}
+                     onChange={e => toggleItem(e.target.checked, item)}/>
             </label>
           </li>
         ))}
