37a958923705954ac5b64d992de47da1227f752d
avrahamm <papaabramg@gmail.com>
2020-07-26 16:49:13 +0300

    react - lesson 16, ex2 fixes

--- 16-exercises/ex2/src/main.js
+++ 16-exercises/ex2/src/main.js
@@ -6,21 +6,27 @@ import ItemsList from './ItemsList';
 
 
 const ListWithFilter = () => {
-    const initialItems = ["a1", "a12", "b1", "b12", "c3", "c34", "d1234", "abc", "dee"]
-    const [pattern, setPattern] = useState("")
+
+    const [pattern, setPattern] = useState("");
   // You don't really need to save itemList in state, do you?
         // Fixed. Removed itemList from state.
 
     function updatePattern(newPattern)
     {
-        setPattern(newPattern)
+        setPattern(newPattern);
+    }
+
+    function getInitialList()
+    {
+        const initialItems = ["a1", "a12", "b1", "b12", "c3", "c34", "d1234", "abc", "dee"];
+        return initialItems;
     }
 
   return (
     <div>
       <Filter updatePattern={updatePattern}/>
       <hr />
-      <ItemsList itemsList={initialItems.filter( (item) => item.includes(pattern))}  />
+      <ItemsList itemsList={getInitialList().filter( (item) => item.includes(pattern))}  />
     </div>
   )
 };
