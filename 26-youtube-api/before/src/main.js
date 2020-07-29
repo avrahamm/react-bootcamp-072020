ea9fed1ac4f13a0440136335ca33fd9fd8836e43
avrahamm <papaabramg@gmail.com>
2020-07-29 17:35:29 +0300

    react - lesson 26, before bonus

--- 26-youtube-api/before/src/main.js
+++ 26-youtube-api/before/src/main.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
+import YouTubePlayer from 'youtube-player';
+import './main.css'
 
 function YoutubePlayer(props) {
-  const [videoId, setVideoId] = useState('');
+  const [videoId, setVideoId] = useState('-B8Q7Nv0yDQ'); // 'RJ1SuQVopjE'
+    const playerDivRef = useRef(null);
+    const playerRef = useRef(null);
+
+    useEffect( function() {
+        playerRef.current = YouTubePlayer(playerDivRef.current);
+    }, []);
+
+    useEffect( function() {
+        const player = playerRef.current;
+        player.loadVideoById(videoId);
+        player.playVideo();
+    }, [videoId]);
 
   return (
     <div>
-      <input type="text" value={videoId} onChange={(e) => setVideoId(e.target.value)} />
-      <div className='player-div' />
+        <div>
+            <input type="text" value={videoId} onChange={(e) => setVideoId(e.target.value)} />
+        </div>
+      <div className='player-div' ref={playerDivRef}/>
     </div>
   );
 }
@@ -19,6 +35,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
+        <YoutubePlayer />
     </div>
   )
 };
