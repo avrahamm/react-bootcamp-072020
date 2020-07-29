607b16d63522ffa8d07ce5c494aba291bf07fa22
avrahamm <papaabramg@gmail.com>
2020-07-29 17:36:06 +0300

    react - lesson 26, before bonus

--- 26-youtube-api/before/src/main.js
+++ 26-youtube-api/before/src/main.js
@@ -1,46 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useState, useEffect, useRef } from 'react';
+import {useState, useEffect, useRef} from 'react';
 import YouTubePlayer from 'youtube-player';
 import './main.css'
 
 function YoutubePlayer(props) {
-  const [videoId, setVideoId] = useState('-B8Q7Nv0yDQ'); // 'RJ1SuQVopjE'
+    const [videoId, setVideoId] = useState('-B8Q7Nv0yDQ'); // 'RJ1SuQVopjE'
     const playerDivRef = useRef(null);
     const playerRef = useRef(null);
 
-    useEffect( function() {
+    useEffect(function () {
         playerRef.current = YouTubePlayer(playerDivRef.current);
     }, []);
 
-    useEffect( function() {
+    useEffect(function () {
         const player = playerRef.current;
         player.loadVideoById(videoId);
         player.playVideo();
     }, [videoId]);
 
-  return (
-    <div>
+    return (
         <div>
-            <input type="text" value={videoId} onChange={(e) => setVideoId(e.target.value)} />
+            <div>
+                <input type="text" value={videoId} onChange={(e) => setVideoId(e.target.value)}/>
+            </div>
+            <div className='player-div' ref={playerDivRef}/>
         </div>
-      <div className='player-div' ref={playerDivRef}/>
-    </div>
-  );
+    );
 }
 
 
 const App = () => {
 
-  return (
-    <div>
-      <h1>Hello World</h1>
-        <YoutubePlayer />
-    </div>
-  )
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <YoutubePlayer/>
+        </div>
+    )
 };
 
 
 // main.js
 const root = document.querySelector('main');
-ReactDOM.render(<App />, root);
+ReactDOM.render(<App/>, root);
