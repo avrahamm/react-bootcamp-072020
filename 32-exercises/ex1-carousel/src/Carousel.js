e1401bdfdaf040eeae5edbc9a9bb3f2f27166166
avrahamm <papaabramg@gmail.com>
2020-08-04 20:58:09 +0300

    react - lesson 32, ex1 p1

--- 32-exercises/ex1-carousel/src/Carousel.js
+++ 32-exercises/ex1-carousel/src/Carousel.js
@@ -4,12 +4,11 @@ import {useState} from 'react';
 export function Carousel(props) {
     const [currentPage, setCurrentPage] = useState(0);
     const numberOfPages = React.Children.count(props.children);
-    const itemStyle = {width:100, height: 100, margin:10};
 
     function pageComponent(pageIndex) {
         const pages = React.Children.toArray(props.children);
         const page = pages[pageIndex];
-        return React.cloneElement(page, {style:itemStyle});
+        return React.cloneElement(page);
     }
 
     return (
