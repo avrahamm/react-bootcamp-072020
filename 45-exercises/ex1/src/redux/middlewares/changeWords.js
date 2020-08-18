cdae94041c8f70ddb020d0de675d287245ce12c4
avrahamm <papaabramg@gmail.com>
2020-08-18 15:26:23 +0300

    react - lesson 45, refactoring

--- 45-exercises/ex1/src/redux/middlewares/changeWords.js
+++
@@ -1,9 +0,0 @@
-const changeWords = store => next => action => {
-    if (action.type === 'RECEIVED_MESSAGE') {
-        console.log('changeWords');
-        action.payload.text = action.payload.text.replace(/angular/g, 'react');
-    }
-    return next(action);
-}
-
-export default changeWords;
--- 45-exercises/ex1/src/redux/middlewares/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = store => next => action => {
-    console.log('Action:', action)
-    return next(action);
-}
-
-export default logger;
