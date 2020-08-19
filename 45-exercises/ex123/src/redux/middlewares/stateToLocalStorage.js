d6f2ca13e9ced463ed3dbc0006d997ecb88bb927
avrahamm <papaabramg@gmail.com>
2020-08-19 16:47:26 +0300

    react - lesson 45, fixing

--- 45-exercises/ex123/src/redux/middlewares/stateToLocalStorage.js
+++ 45-exercises/ex123/src/redux/middlewares/stateToLocalStorage.js
@@ -1,5 +1,7 @@
 const stateToLocalStorage = store => next => action => {
+    // First let action complete to modify state.
     const result = next(action);
+    // Then, save state to recover after refresh.
     const localStorage = window.localStorage;
     const state = store.getState();
     // console.log(JSON.stringify(state))
--- 45-exercises/ex123/src/redux/middlewares/undo.js
+++ 45-exercises/ex123/src/redux/middlewares/undo.js
@@ -1,18 +1,24 @@
 let pastStates = [];
 const undo = store => next => action => {
     if (  action.type === 'UNDO') {
+        // debugger
         if ( pastStates.length === 0 ) {
             return;
         }
         const prevState = pastStates.pop();
         action.payload = prevState;
+        console.log('pastStates', pastStates);
+        return next(action);
     }
     else {
+        // debugger
+        // save state BEFORE action changed state,
+        // so when 'UNDO' action comes returns to that state.
         const state = store.getState();
         pastStates.push(state);
+        console.log('pastStates', pastStates);
+        return next(action);
     }
-    console.log('pastStates', pastStates);
-    return next(action);
 }
 
 export default undo;
