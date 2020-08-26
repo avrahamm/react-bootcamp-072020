39defe43eb298661c0449eb5f88d3aa0d1059532
avrahamm <papaabramg@gmail.com>
2020-08-26 13:26:43 +0300

    chat project - imports

--- chat-project/src/main.js
+++ chat-project/src/main.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "jquery";
 import Login from './Components/Login/Login'
 import MainScreen from "./Components/MainScreen/MainScreen";
-// import "bootstrap/dist/css/bootstrap.min.css"
 
 const App = () => {
     // const [user, setUser] = useState(null);
