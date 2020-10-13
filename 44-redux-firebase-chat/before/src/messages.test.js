6fc07efe7a18099a83fc28f84955ceda730c91aa
avrahamm <papaabramg@gmail.com>
2020-10-13 14:30:29 +0300

    44-redux-firebase-chat/before ynonp configuration fix for rtl async tests

--- 44-redux-firebase-chat/before/src/messages.test.js
+++ 44-redux-firebase-chat/before/src/messages.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {createStore} from 'redux'
 import {Provider} from 'react-redux';
 
-import {screen, render, wait} from '@testing-library/react'
+import {screen, render} from '@testing-library/react'
 import Messages from './messages.js'
 import reducer from './redux/reducers/index';
 import store from './redux/store';
@@ -11,7 +11,7 @@ import store from './redux/store';
 // jest.mock('./redux/store');
 
 describe('Messages component', () => {
-    it.skip('should have "Messages" label in the dom',
+    it('should have "Messages" label in the dom',
         () => {
             // console.log(store.getState());
             const {container, getByText} = render(
@@ -21,49 +21,49 @@ describe('Messages component', () => {
             );
             expect(getByText(/messages/i))
                 .toBeInTheDocument();
-
         })
+
+    test('assure number of messages in the dom',
+        async () => {
+
+            const initialState = {
+                messages: {
+                    messages: [
+                        {
+                            id: 'QaJmVJ9saVYOgtgOyiuQ',
+                            from: "a1",
+                            text: "Ha1"
+                        },
+                        {
+                            id: 'QaJmVJ9saVYOgtgOyirr',
+                            from: "a2",
+                            text: "Ha2"
+                        }
+                    ]
+                },
+                rooms: {rooms: [], activeRoomId: 0},
+                account: {username: 'guest'}
+            };
+
+            const myStore = createStore(reducer, initialState);
+
+            const {
+                getByText, getAllByRole,
+                queryAllByRole,
+                findByText, findAllByRole
+            } = render(
+                <Provider store={myStore}>
+                    <Messages/>
+                </Provider>
+            );
+
+            const messagesByRole = await findAllByRole("message-item");
+            expect(messagesByRole).toHaveLength(2);
+            const messageByText = await findByText(/ha1/i);
+            expect(messageByText).toBeInTheDocument();
+        })
+
 })
 
 
-test('assure number of messages in the dom',
-    async () => {
-
-        const messagesCount = 1; //initialState.messages.messages.length;
-        const initialState = {
-            messages: {
-                messages: [
-                    {
-                        id: 'QaJmVJ9saVYOgtgOyiuQ',
-                        from: "a1",
-                        text: "Ha1"
-                    },
-                    {
-                        id: 'QaJmVJ9saVYOgtgOyirr',
-                        from: "a2",
-                        text: "Ha2"
-                    }
-                ]
-            },
-            rooms: {rooms: [], activeRoomId: 0},
-            account: {username: 'guest'}
-        };
-
-        const myStore = createStore(reducer, initialState);
-
-        const {
-            getByText, getAllByRole,
-            queryAllByRole,
-            findByText, findAllByRole
-        } = render(
-            <Provider store={myStore}>
-                <Messages/>
-            </Provider>
-        );
-
-        const messagesByRole = await findAllByRole("message-item");
-        expect(messagesByRole).toHaveLength(2);
-        const messageByText = await findByText(/ha1/i);
-        expect(messageByText).toBeInTheDocument();
-    })
 
